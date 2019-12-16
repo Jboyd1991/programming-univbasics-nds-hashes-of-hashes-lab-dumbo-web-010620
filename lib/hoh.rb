9e1c437b53e8839880f816342cbff7d01ce87204
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-16 03:39:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,16 +31,6 @@
 # }
 
 def naming_system
-  portion_1 = {
-  :label => "Kingdom", {
-   :sub_category => {
-     :label => "Phylum", {
-     :sub_category => {
-       :label => "Class"
-     }
-   }
- }
-  naming_system
   # Remember:
   #  Kingdom
   #  Phylum
